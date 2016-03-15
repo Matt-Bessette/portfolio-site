8254f80e949d40dc9ffe34e97ba4a9ef52e28d8a
Matt Bessette <blade30912@gmail.com>
2016-03-14 23:17:09 -0400

    Got index page styling finished

--- build/index.php
+++ build/index.php
@@ -14,7 +14,20 @@ require_once 'includes/Definitions.php';
 	</head>
 	<body>
 		<?php echo file_get_contents(HEADER); ?>
-		
+		<div class="portfolio">
+			<div>
+				<span><img src="http://ratemyprofessors.mtvnimages.com/prof/t_Patrick_Logan_813936.jpeg"></span>
+				<span>Cotton candy gummies chocolate dessert tart jujubes caramels tiramisu. Pastry tiramisu candy canes marzipan candy canes chupa chups danish. Brownie carrot cake jujubes candy jujubes sweet roll cupcake dragée. Sesame snaps pudding sweet pie danish croissant. Chocolate cake chocolate bar brownie topping dragée fruitcake oat cake croissant. Candy canes candy chocolate donut pastry croissant. Biscuit donut sugar plum tootsie roll oat cake. Jelly-o jujubes caramels bonbon candy biscuit apple pie danish soufflé. Sweet marshmallow liquorice brownie caramels chupa chups chocolate jelly-o. Gingerbread apple pie wafer. Biscuit cotton candy halvah sesame snaps. Pudding lollipop cupcake danish marshmallow gingerbread toffee tiramisu sesame snaps. Chocolate bar tart carrot cake macaroon.</span>
+			</div>
+			<div>
+				<span>
+					Gingerbread sweet gummi bears cotton candy. Donut sweet macaroon liquorice ice cream donut. Danish cheesecake topping muffin sweet roll biscuit chupa chups. Jujubes bonbon pie gingerbread chupa chups. Sesame snaps pudding liquorice pie gingerbread chocolate. Icing caramels cotton candy. Lemon drops sweet roll apple pie soufflé sweet roll. Muffin bonbon macaroon. Marzipan marzipan apple pie toffee jujubes. Apple pie jelly lollipop caramels sesame snaps cake pudding. Biscuit cotton candy lemon drops brownie gummi bears brownie macaroon liquorice. Jelly-o gingerbread pastry.
+				</span>
+				<span>
+					Sweet danish chocolate bar soufflé. Croissant donut brownie cupcake chupa chups. Chupa chups pastry soufflé bonbon chocolate. Cupcake pastry croissant. Pastry chocolate tiramisu macaroon powder jelly bear claw. Cupcake bonbon jelly beans. Wafer jelly liquorice chocolate cake. Caramels chupa chups sweet roll jelly soufflé toffee lollipop. Pie pastry chupa chups ice cream ice cream bonbon. Lollipop pudding liquorice bonbon powder icing candy. Jelly-o jujubes sweet roll icing pastry cake lollipop chupa chups tiramisu. Lollipop powder gummies cupcake muffin tootsie roll chocolate tootsie roll. Cupcake caramels topping jujubes macaroon. Wafer lollipop dragée icing gingerbread.
+				</span>
+			</div>
+		</div>
 		<?php echo file_get_contents(FOOTER); ?>
 	</body>
 </html>

58138435332b7ea921e8132de17e11fc899e0221
Matt Bessette <blade30912@gmail.com>
2016-03-29 20:38:30 -0400

    Removed external folder

--- build/projects.php
+++ build/projects.php
@@ -46,8 +46,7 @@ try {
 		<link rel="stylesheet" type="text/css" href="css/normalize.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 		<link rel="stylesheet" type="text/css" href="css/projects.min.css">
-		<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
-		<script src="projects.js"></script>
+		<script src="lib/jquery.min.js"></script>
 	</head>
 	<body>
 		<?php require_once(HEADER); ?>
---
+++ src/scss/README.md
@@ -0,0 +1,11 @@
+To build, requires folder in this directory called:
+	external/
+		base/ 			Bourbon bitters
+		neat/			Bourbon neat
+		bourbon/		Bourbon
+		fontawesome/	FontAwesome
+
+http://fortawesome.github.io/Font-Awesome/
+http://bitters.bourbon.io
+http://bourbon.io
+http://neat.bourbon.io
